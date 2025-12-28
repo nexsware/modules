1c6b9bec08ca1bd5cb8b0d0dbb498a1e92f24341
sinnedos <sinnedos@proton.me>
2025-12-28 01:19:57 -0600

    test

--- terraform/linode/instance/main.tf
+++ terraform/linode/instance/main.tf
@@ -15,11 +15,17 @@ resource "linode_instance" "this" {
   stackscript_id  = var.stackscript_id != null ? var.stackscript_id : null
 
   # Only include stackscript_data if stackscript_id is provided
-  stackscript_data = var.stackscript_id != null ? {
-    install_nginx       = var.install_nginx ? "true" : "false"
-    server_name         = var.server_name
-    domains_containers  = var.domains_containers
-  } : {}
+  stackscript_data = var.stackscript_id != null ? merge(
+    {
+      install_nginx = var.install_nginx ? "true" : "false"
+    },
+    var.install_nginx && var.server_name != "" ? {
+      server_name = var.server_name
+    } : {},
+    var.install_nginx && var.domains_containers != "" ? {
+      domains_containers = var.domains_containers
+    } : {}
+  ) : {}
 
 
   # Optionally create a public interface
