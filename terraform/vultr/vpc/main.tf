a8074d2d7743f5ce583a00914121048388062680
sinnedos <sinnedos@proton.me>
2026-05-02 22:18:44 -0500

    update modules

--- terraform/vultr/vpc/main.tf
+++ terraform/vultr/vpc/main.tf
@@ -2,9 +2,9 @@ provider "vultr" {
   api_key = var.vultr_api_key
 }
 
-resource "vultr_vpc2" "this" {
+resource "vultr_vpc" "this" {
   region        = var.region
   description   = var.description
-  ip_block      = var.ip_block
-  prefix_length = var.prefix_length
+  v4_subnet      = var.ip_block
+  v4_subnet_mask = var.prefix_length
 }
--- terraform/vultr/vpc/outputs.tf
+++ terraform/vultr/vpc/outputs.tf
@@ -1,24 +1,24 @@
 output "id" {
   description = "The ID of the VPC 2.0"
-  value       = vultr_vpc2.this.id
+  value       = vultr_vpc.this.id
 }
 
 output "region" {
   description = "The region where the VPC is deployed"
-  value       = vultr_vpc2.this.region
+  value       = vultr_vpc.this.region
 }
 
 output "ip_block" {
   description = "The IPv4 network address of the VPC subnet"
-  value       = vultr_vpc2.this.ip_block
+  value       = vultr_vpc.this.v4_subnet
 }
 
 output "prefix_length" {
   description = "The prefix length of the VPC subnet"
-  value       = vultr_vpc2.this.prefix_length
+  value       = vultr_vpc.this.v4_subnet_mask
 }
 
 output "description" {
   description = "The description of the VPC"
-  value       = vultr_vpc2.this.description
+  value       = vultr_vpc.this.description
 }
