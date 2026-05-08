e07166390371b59e95928e487bac1338551d8ab3
sinnedos <sinnedos@proton.me>
2026-05-07 21:11:32 -0500

    update

--- terraform/vultr/object-storage/outputs.tf
+++ terraform/vultr/object-storage/outputs.tf
@@ -3,24 +3,17 @@ output "id" {
   value       = vultr_object_storage.this.id
 }
 
-output "s3_hostname" {
-  description = "S3-compatible endpoint hostname"
-  value       = vultr_object_storage.this.s3_hostname
-}
-
-output "s3_access_key" {
-  description = "S3 access key"
-  value       = vultr_object_storage.this.s3_access_key
-  sensitive   = true
-}
-
-output "s3_secret_key" {
-  description = "S3 secret key"
-  value       = vultr_object_storage.this.s3_secret_key
-  sensitive   = true
-}
-
 output "label" {
   description = "The label of the object storage instance"
   value       = vultr_object_storage.this.label
+}
+
+output "region" {
+  description = "The region where the object storage instance is deployed"
+  value       = vultr_object_storage.this.region
+}
+
+output "status" {
+  description = "The status of the object storage instance"
+  value       = vultr_object_storage.this.status
 }
