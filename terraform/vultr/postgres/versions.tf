d0546c8ece13bc489dcfc1374e86a8f2c7682576
sinnedos <sinnedos@proton.me>
2026-05-08 11:09:51 -0500

    test

--- terraform/vultr/postgres/versions.tf
+++ terraform/vultr/postgres/versions.tf
@@ -1,7 +1,20 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.6"
 
-  backend "s3" {}
+  backend "s3" {
+    region = "us-east-1"
+
+    endpoints = {
+      s3 = "https://ams1.vultrobjects.com"
+    }
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
 
   required_providers {
     vultr = {
